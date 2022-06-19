67165a6ca16ab9543d4bdf946764176c48c10d07
test <test>
2022-06-19 18:01:10 +0700

    _

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express'
 import {Types} from 'mongoose'
 import Orders from '../models/Orders'
+import * as nodemailer from 'nodemailer'
 
 const get = async (req: Request, res: Response) => {
 	const id = new Types.ObjectId(req.params.id)
@@ -81,4 +82,29 @@ const update = async (req: Request, res: Response) => {
 	}
 }
 
-export default {get, list, create, remove, update}
+
+const send = async (req: Request, res: Response) => {
+	const transporter = nodemailer.createTransport({
+		host: 'smtp.yandex.ru',
+		port: 465,
+		secure: true,
+		auth: {
+		  user: 'doctor-maxin@yandex.ru',
+		  pass: 'lnwswhvndbqlmxgi',
+		},
+	  })
+
+	  let result = await transporter.sendMail({
+		from: '"Node js" <nodejs@example.com>',
+		to: 'user@example.com, user@example.com',
+		subject: 'Message from Node js',
+		text: 'This message was sent from Node js server.',
+		html:
+		  'This <i>message</i> was sent from <strong>Node js</strong> server.',
+	  })
+	  console.log(result)
+	  
+}
+
+
+export default {get, list, create, remove, update, send}
--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -3,6 +3,7 @@ import controller from '../controllers/orders'
 
 const router = express.Router()
 
+router.get('/send', controller.send)
 router.get('/:id', controller.get)
 router.delete('/:id', controller.remove)
 router.put('/:id', controller.update)
